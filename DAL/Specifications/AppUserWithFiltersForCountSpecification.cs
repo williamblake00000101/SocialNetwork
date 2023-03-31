2d8a2e752bb097285489b1027ea3243f8f75e8b9
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-03-31 08:27:03 +0300

    add Specifications

---
+++ DAL/Specifications/AppUserWithFiltersForCountSpecification.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Entities;
+
+namespace DAL.Specifications;
+
+public class AppUserWithFiltersForCountSpecification : BaseSpecification<AppUser>
+{
+    public AppUserWithFiltersForCountSpecification(UserParams userParams) : base(a =>
+        (string.IsNullOrEmpty(userParams.Search) || a.LastName.ToLower().Contains(userParams.Search)) &&
+        (!userParams.SpecializationId.HasValue || a.SpecializationId == userParams.SpecializationId))
+    {
+    }
+}
---
+++ DAL/Specifications/AppUserWithSpecializationSpecification.cs
@@ -0,0 +1,36 @@
+﻿using DAL.Entities;
+
+namespace DAL.Specifications;
+
+public class AppUserWithSpecializationSpecification: BaseSpecification<AppUser>
+{
+    public AppUserWithSpecializationSpecification(UserParams userParams) : base(a =>
+        (string.IsNullOrEmpty(userParams.Search) || a.LastName.ToLower().Contains(userParams.Search)) &&
+        (!userParams.SpecializationId.HasValue || a.SpecializationId == userParams.SpecializationId))
+    {
+        AddInclude(a => a.Specialization);
+        AddOrderBy(a => a.UserName);
+        ApplyPaging(userParams.PageSize * (userParams.PageNumber - 1), userParams.PageSize);
+
+        if (!string.IsNullOrEmpty(userParams.Sort))
+        {
+            switch (userParams.Sort)
+            {
+                case "yearDateOfBirthAsc":
+                    AddOrderBy(a => a.DateOfBirth);
+                    break;
+                case "yearDateOfBirthDesc":
+                    AddOrderByDescending(a => a.DateOfBirth);
+                    break;
+                default:
+                    AddOrderBy(a => a.UserName);
+                    break;
+            }
+        }
+    }
+
+    public AppUserWithSpecializationSpecification(int id) : base(x => x.Id == id)
+    {
+        AddInclude(a => a.Specialization);
+    }
+}
