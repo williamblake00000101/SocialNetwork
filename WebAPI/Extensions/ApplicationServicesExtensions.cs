f978e32ad225f77877bf6fc2c55e9c21761f1dd0
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-03-31 09:41:05 +0300

    add ServicesExtensions...

---
+++ WebAPI/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,31 @@
+﻿using DAL;
+using DAL.Context;
+using DAL.Interfaces;
+using DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Extensions;
+
+public static class ApplicationServicesExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddDbContext<SocialNetworkContext>(options =>
+        {
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        });
+        
+        services.AddCors();
+        //services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        
+        
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+        
+        return services;
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,24 +1,32 @@
+using WebAPI.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddSwaggerDocumentation();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwaggerDocumentation();
 
-app.UseHttpsRedirection();
+app.UseCors(builder => builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("https://localhost:4200"));
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.MapControllers();
 
