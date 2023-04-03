ca60b62161c6f33828ecc2f0e49991b57df39880
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-04-03 07:56:39 +0300

    add Extensions...

---
+++ BLL/Extensions/UserManagerExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BLL.Extensions;
+
+public static class UserManagerExtensions
+{
+    public static async Task<AppUser> FindUserByClaimsPrincipleWithAddress(this UserManager<AppUser> userManager,
+        ClaimsPrincipal user)
+    {
+        var email = user.FindFirstValue(ClaimTypes.Email);
+
+        return await userManager.Users.Include(x => x.Photos)
+            .SingleOrDefaultAsync(x => x.Email == email);
+    }
+
+    public static async Task<AppUser> FindByEmailFromClaimsPrincipal(this UserManager<AppUser> userManager,
+        ClaimsPrincipal user)
+    {
+        return await userManager.Users
+            .SingleOrDefaultAsync(x => x.Email == user.FindFirstValue(ClaimTypes.Email));
+    }
+}
