c6154000618999b98c79c0e83fe01959ffca4af5
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-04-22 09:13:38 +0300

    add messages...

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,22 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AdminGuard } from './core/guards/admin.guard';
-import { AuthGuard } from './core/guards/auth.guard';
-import { PreventUnsavedChangesGuard } from './core/guards/prevent-unsaved-changes.guard';
-import { NotFoundComponent } from './core/not-found/not-found.component';
-import { MemberDetailedResolver } from './core/resolver/member-detailed.resolver';
-import { ServerErrorComponent } from './core/server-error/server-error.component';
-import { TestErrorComponent } from './core/test-error/test-error.component';
-import { HomeComponent } from './home/home/home.component';
-import { ListsComponent } from './lists/lists.component';
-import { MemberDetailComponent } from './members/member-detail/member-detail.component';
-import { MemberEditComponent } from './members/member-edit/member-edit.component';
-import { MemberListComponent } from './members/member-list/member-list.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AdminGuard} from './core/guards/admin.guard';
+import {AuthGuard} from './core/guards/auth.guard';
+import {PreventUnsavedChangesGuard} from './core/guards/prevent-unsaved-changes.guard';
+import {NotFoundComponent} from './core/not-found/not-found.component';
+import {MemberDetailedResolver} from './core/resolver/member-detailed.resolver';
+import {ServerErrorComponent} from './core/server-error/server-error.component';
+import {TestErrorComponent} from './core/test-error/test-error.component';
+import {HomeComponent} from './home/home/home.component';
+import {ListsComponent} from './lists/lists.component';
+import {MemberDetailComponent} from './members/member-detail/member-detail.component';
+import {MemberEditComponent} from './members/member-edit/member-edit.component';
+import {MemberListComponent} from './members/member-list/member-list.component';
+import {MessagesComponent} from './messages/messages.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
-
-  {path: '',
+  {
+    path: '',
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
@@ -24,16 +25,18 @@ const routes: Routes = [
       {path: 'members/:username', component: MemberDetailComponent, resolve: {member: MemberDetailedResolver}},
       {path: 'member/edit', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
       {path: 'lists', component: ListsComponent},
-
-
-      {path: 'admin', canActivate: [AdminGuard],
-        loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), data: {breadcrumb: {skip: true}}}
+      {path: 'messages', component: MessagesComponent},
+      {
+        path: 'admin', canActivate: [AdminGuard],
+        loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), data: {breadcrumb: {skip: true}}
+      }
     ]
   },
-
-
-
-  {path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule), data: {breadcrumb: {skip: true}}},
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then(m => m.AccountModule),
+    data: {breadcrumb: {skip: true}}
+  },
   {path: 'test-error', canActivate: [AdminGuard], component: TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
   {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
   {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Errors'}},
@@ -44,4 +47,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- ClientApp/src/app/messages/messages.component.ts
+++ ClientApp/src/app/messages/messages.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Message} from '../shared/models/message';
+import {MessageService} from './messages.service';
 
 @Component({
   selector: 'app-messages',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class MessagesComponent {
 
+  messages?: Message[];
+  container = 'Unread';
+  pageNumber = 1;
+  pageSize = 10;
+  loading = false;
+
+  constructor(private messageService: MessageService) {
+  }
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.loading = true;
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe({
+      next: response => {
+        this.messages = response.result;
+        this.loading = false;
+      }
+    })
+  }
+
+  deleteMessage(id: number) {
+    this.messageService.deleteMessage(id).subscribe({
+      next: () => this.messages?.splice(this.messages.findIndex(m => m.id === id), 1)
+    })
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 }
